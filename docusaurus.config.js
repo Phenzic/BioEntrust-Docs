eea9f93dca8f99be17d82205acefe7d93758c52d
Phenzic <juliuayoola5@gmail.com>
2024-01-28 04:44:41 +0100

    some changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Documentation',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
